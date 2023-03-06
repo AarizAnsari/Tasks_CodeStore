784249e69ad5e31a2c68d60817e984ea532e4a98
Aariz Ansari <aaariz60@gmail.com>
2023-03-06 15:51:15 +0530

    Update JS_Area_Class.js

--- 06-03-2023/JS_Area_Class.js
+++ 06-03-2023/JS_Area_Class.js
@@ -1,16 +1,21 @@
-
+//DECLARING AREA CLASS
 class Area {
+
+    //METHOD TO CALCULATE AREA OF SQUARE
    static square(length) {
         return length * length;
     }
+
+    //METHOD TO CALCULATE AREA OF RECTANGLE
    static rectangle(length, breadth) {
         return length * breadth;
     }
+
+    //METHOD TO CALCULATE AREA OF TRIANGLE
    static triangle(base, height) {
         return (base * height) / 2;
     }
 }
-
 
 console.log(Area.square(2));
 
