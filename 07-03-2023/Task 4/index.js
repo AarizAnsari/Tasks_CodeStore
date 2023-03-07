139e002d18fb4f3cd24384c56d530eb18418c542
Aariz Ansari <aaariz60@gmail.com>
2023-03-07 16:08:35 +0530

    Update index.js

--- 07-03-2023/Task 4/index.js
+++ 07-03-2023/Task 4/index.js
@@ -2,80 +2,78 @@
 var source = "";
 var destination = "";
 
+//GLOBAL OBJECT TO STORE SELECTED ROUTES
 const routes = {};
-
-//SOURCE CITY OBJECT
-//VALUE DEFINES A BOOLEAN TO IDENTIFY THE CITY IS  DISABLED OR ENABLED
-
-
 
 //FUNCTION TO DISABLE DESTINATION CITY WHEN SELECTED IN SOURCE
 function setSource(dest) {
-    
+
+    //SOURCE CITY OBJECT
+    //VALUE DEFINES A BOOLEAN TO IDENTIFY THE CITY IS  DISABLED OR ENABLED
     const source_cities = {
-        delhi : false,
-        noida : false,
-        lucknow :false,
-        roorkee :false,
-        mumbai : false,
-        jaipur : false
+        delhi: false,
+        noida: false,
+        lucknow: false,
+        roorkee: false,
+        mumbai: false,
+        jaipur: false
     }
 
     let keys = Object.keys(source_cities);
     let x = document.getElementById('source').value;
     source_cities[x] = true;
 
-    if(dest != 'none'){
+    if (dest != 'none') {
         source_cities[dest] = true;
     }
-    for(let i=0;i<=5;i++){
-        document.getElementById('destination').options[i+1].disabled = source_cities[keys[i]];
+    for (let i = 0; i <= 5; i++) {
+        document.getElementById('destination').options[i + 1].disabled = source_cities[keys[i]];
     }
-    document.getElementById('from').innerHTML = ("From :"+x).toUpperCase();
+    document.getElementById('from').innerHTML = ("From :" + x).toUpperCase();
     source = x;
 }
 
 //FUNCTION TO DISABLE SOURCE CITY WHEN SELECTED IN DESTINATION
 function setDestination(src) {
-    
+
     const dest_cities = {
-        delhi : false,
-        noida : false,
-        lucknow :false,
-        roorkee :false,
-        mumbai : false,
-        jaipur : false
-    }    
+        delhi: false,
+        noida: false,
+        lucknow: false,
+        roorkee: false,
+        mumbai: false,
+        jaipur: false
+    }
 
     let keys = Object.keys(dest_cities);
     let x = document.getElementById('destination').value;
     dest_cities[x] = true;
 
-    if(src != 'none'){
+    if (src != 'none') {
         dest_cities[src] = true;
     }
 
-    for(let i=0;i<=5;i++){
-        document.getElementById('source').options[i+1].disabled = dest_cities[keys[i]];
+    for (let i = 0; i <= 5; i++) {
+        document.getElementById('source').options[i + 1].disabled = dest_cities[keys[i]];
     }
-    document.getElementById('to').innerHTML = ("To :"+x).toUpperCase();
+    document.getElementById('to').innerHTML = ("To :" + x).toUpperCase();
     destination = x;
 }
 
 //FUNTION TO SET SELCTED SOURCE AND DESTINATION TO A ROUTE
-function setRoute(){
+function setRoute() {
 
     //ERROR HANDLING IN CASE CITY OR DESTINATION IS NOT SELECTED
-    try{
-        if(destination==''||source==''){
+    try {
+        if (destination == '' || source == '') {
             throw "Please select destination and source"
-        } 
+        }
         routes[source] = destination;
-        destination ="";
+        destination = "";
         source = "";
-        let data  = "Created Routes <br>";
+        let data = "Created Routes <br>";
         for (const key in routes) {
-            data += ("from :"+key+"&nbsp"+"to :"+routes[key]+"<br>"); 
+            data += ("from :" + key + "&nbsp" + "to :" + routes[key] + "<br>");
             setDestination(routes[key]);
             setSource(key);
         }
@@ -83,7 +81,7 @@ function setRoute(){
         document.getElementById('to').innerHTML = null;
         document.getElementById('from').innerHTML = null;
     }
-    catch(err){
+    catch (err) {
         window.alert(err);
     }
 }
