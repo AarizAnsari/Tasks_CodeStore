0f34d95287179186d2bd7d848d16aba755d69c1c
Aariz Ansari <aaariz60@gmail.com>
2023-03-14 10:38:35 +0530

    Update Task_3.ts

--- 13-03-2023/Task_3.ts
+++ 13-03-2023/Task_3.ts
@@ -3,7 +3,7 @@ const array: (string|number)[] = ["1", "a", "2", "b", "3", "c", "4", "d", "5", "
 array.forEach(
   function (value) {
   if (isNaN(Number(value))) {
-    console.log(value.toUpperCase());
+    console.log(value.toString().toUpperCase());
   } 
   else {
     value = Number(value);
