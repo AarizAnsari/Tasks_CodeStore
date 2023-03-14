fef6ef3e01b97e4e7c2bda2c66ad8c807a5381fc
Aariz Ansari <aaariz60@gmail.com>
2023-03-14 17:47:33 +0530

    minor changes

--- 14-03-2023/Charts/Self_Made/index.js
+++ 14-03-2023/Charts/Self_Made/index.js
@@ -1,38 +1,44 @@
+//DATA IN FORM OF AN OBJECT
 const data = {
     Physics: 70,
     Chemistry: 80,
     Maths: 50,
     English: 100,
-  };
+};
 
-  const colors = ["red","green","blue","burlywood"];
+//COLOR ARRAY TO GIVE DIFFERENT COLORS TO DIFFERENT BARS
+const colors = ["red", "green", "blue", "burlywood"];
 
-  const quote = '"';
-  let chart = "";
-  let max = 0;
-  let i = 0;
-  for (const key in data) {
-    if(data[key]>max){max=data[key];}
-    if(i>= colors.length){i=0;}
+const quote = '"';
+let chart = "";
+let max = 0;
+let i = 0;
+for (const key in data) {
+    if (data[key] > max) { max = data[key]; }
+
+    //RESETTING THE VALUE OF i IF ENTRIES ARE GREATER THAN SIZE OF COLOR ARRAY
+    if (i >= colors.length) { i = 0; }
+
+    //INSERTING DIV AS STRING IN DOCUMENT 
     chart +=
-      "<div style = " +
-      quote +
-      " height:20px;width:" +
-      (data[key] * 2).toString() +
-      "px;background-color:"+
-      colors[i]
-      +";margin: 5px" +
-      quote +
-      ">" +
-      key +
-      "</div>";
+        "<div style = " +
+        quote +
+        " height:20px;width:" +
+        (data[key] * 2).toString() +
+        "px;background-color:" +
+        colors[i]
+        + ";margin: 5px" +
+        quote +
+        ">" +
+        key +
+        "</div>";
     i++;
-  }
-   
-  document.getElementById("chart").innerHTML = chart;
+}
 
-  document.getElementById("chart").style.borderBottom = "1px dashed black";
+document.getElementById("chart").innerHTML = chart;
 
-  document.getElementById("chart").style.borderLeft = "1px dashed black";
+document.getElementById("chart").style.borderBottom = "1px dashed black";
 
-  document.getElementById("chart").style.width = ((max*2)+5).toString()+"px";
+document.getElementById("chart").style.borderLeft = "1px dashed black";
+
+document.getElementById("chart").style.width = ((max * 2) + 5).toString() + "px";
--- 14-03-2023/Charts/Using_Library/index.js
+++ 14-03-2023/Charts/Using_Library/index.js
@@ -1,108 +1,79 @@
 
-      // Load the Visualization API and the corechart package.
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.load('current', {'packages':['timeline']});
-      google.charts.load('current', {'packages':['table']});
-      // Set a callback to run when the Google Visualization API is loaded.
-      google.charts.setOnLoadCallback(drawPieChart);
-      google.charts.setOnLoadCallback(drawTable);
-      google.charts.setOnLoadCallback(drawTimeleine);
+// Load the Visualization API and the corechart package.
+google.charts.load('current', { 'packages': ['corechart'] });
+google.charts.load('current', { 'packages': ['timeline'] });
+google.charts.load('current', { 'packages': ['table'] });
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawPieChart);
+google.charts.setOnLoadCallback(drawTable);
+google.charts.setOnLoadCallback(drawTimeleine);
 
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
-      function drawPieChart() {
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
 
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'activity');
-        data.addColumn('number', 'distribution');
-        data.addRows([
-          ['Coding', 4],
-          ['Research', 1],
-          ['Studying', 2],
-          ['Bug Fixing', 2]
-        ]);
+//FUNCTION TO DRAW TIMELINE CHART
+function drawTimeleine() {
+  var container = document.getElementById('timeline');
+  var chart = new google.visualization.Timeline(container);
+  var dataTable = new google.visualization.DataTable();
 
-        // Set chart options
-        var options = {'title':'Activity Pie Chart',
-                       'width':800,
-                       'height':400};
+  dataTable.addColumn({ type: 'string', id: 'Task' });
+  dataTable.addColumn({ type: 'date', id: 'Start' });
+  dataTable.addColumn({ type: 'date', id: 'End' });
+  dataTable.addRows([
+    ['Correction', new Date("March 14 2023 10:00"), new Date("March 14 2023 11:00")],
+    ['Generic Type', new Date("March 14 2023 11:00"), new Date("March 14 2023 13:00")],
+    ['Charts', new Date("March 14 2023 13:10"), new Date("March 14 2023 14:00")],
+    ['Lunch', new Date("March 14 2023 14:00"), new Date("March 14 2023 15:00")],
+    ['Charts', new Date("March 14 2023 15:00"), new Date("March 14 2023 18:00")],
+  ]);
 
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div1'));
-        chart.draw(data, options);
-      }
-
-      function drawTable() {
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Task');
-        data.addColumn('string', 'Duration');
-        data.addColumn('boolean', 'Status');
-        data.addRows([
-          ['Correction','01:00', true],
-          ['Generic Types','02:00', true],
-          ['Lunch','01:00', true],
-          ['Charts','02:00', false],
-        ]);
-
-        var table = new google.visualization.Table(document.getElementById('table_div'));
-
-        table.draw(data, {showRowNumber: true,width : '1000px',height: '300px'});
-      }
-
-      function drawTimeleine() {
-        var container = document.getElementById('timeline');
-        var chart = new google.visualization.Timeline(container);
-        var dataTable = new google.visualization.DataTable();
-
-        dataTable.addColumn({ type: 'string', id: 'Task' });
-        dataTable.addColumn({ type: 'date', id: 'Start' });
-        dataTable.addColumn({ type: 'date', id: 'End' });
-        dataTable.addRows([
-          [ 'Correction', new Date("March 14 2023 10:00"),new Date("March 14 2023 11:00")],
-          [ 'Generic Type', new Date("March 14 2023 11:00"),new Date("March 14 2023 13:00")],
-          [ 'Charts', new Date("March 14 2023 13:10"),new Date("March 14 2023 14:00")],
-          [ 'Lunch', new Date("March 14 2023 14:00"),new Date("March 14 2023 15:00")],
-          [ 'Charts', new Date("March 14 2023 15:00"),new Date("March 14 2023 18:00")],
-        ]);
-
-        chart.draw(dataTable);
-      }
+  chart.draw(dataTable);
+}
 
 
-      function drawChart() {
-        const dataTable = new google.visualization.DataTable();
-        dataTable.addColumn({type: 'string', 'id': 'category'});
-        dataTable.addColumn({type: 'number', 'id': 'amount'});
-        dataTable.addRows([
-          ['A', 28],
-          ['B', 55],
-          ['C', 43],
-          ['D', 91],
-          ['E', 81],
-          ['F', 53],
-          ['G', 19],
-          ['H', 87],
-        ]);
+//FUNCTION TO DRAW PIE CHART
+function drawPieChart() {
 
-        const options = {
-          "vega": {
-            "$schema": "https://vega.github.io/schema/vega/v4.json",
-            "width": 500,
-            "height": 200,
-            "padding": 5,
+  // Create the data table.
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'activity');
+  data.addColumn('number', 'distribution');
+  data.addRows([
+    ['Coding', 4],
+    ['Research', 1],
+    ['Studying', 2],
+    ['Bug Fixing', 2]
+  ]);
 
-            'data': [{'name': 'table', 'source': 'datatable'}],
+  // Set chart options
+  var options = {
+    'title': 'Activity Pie Chart',
+    'width': 800,
+    'height': 400
+  };
 
-            "axes": [
-              { "orient": "bottom", "scale": "xscale" },
-              { "orient": "left", "scale": "yscale" }
-            ],
+  // Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.PieChart(document.getElementById('chart_div1'));
+  chart.draw(data, options);
+}
 
-          }
-        };
 
-        const chart = new google.visualization.VegaChart(document.getElementById('chart-div'));
-        chart.draw(dataTable, options);
-      }
+//FUNCTION TO DRAW TABLE
+function drawTable() {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Task');
+  data.addColumn('string', 'Duration');
+  data.addColumn('boolean', 'Status');
+  data.addRows([
+    ['Correction', '01:00', true],
+    ['Generic Types', '02:00', true],
+    ['Lunch', '01:00', true],
+    ['Charts', '02:00', false],
+  ]);
+
+  var table = new google.visualization.Table(document.getElementById('table_div'));
+
+  table.draw(data, { showRowNumber: true, width: '1000px', height: '300px' });
+}
